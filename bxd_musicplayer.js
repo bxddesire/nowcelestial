2e3223761c9cc0ed5092544faa705f9b1da4debe
bxd desire <nowcelestial@gmail.com>
2026-06-22 13:54:23 +0200

    Create bxd_musicplayer.js

---
+++ bxd_musicplayer.js
@@ -0,0 +1,6 @@
+(function () {
+  var s = document.createElement('script');
+  s.src = 'https://www.aht.li/3931788/music_player_script.js';
+  s.defer = true;
+  document.head.appendChild(s);
+})();
